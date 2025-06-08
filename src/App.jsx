40abc857ec99ff2e9cdab8bb2e765bf524196cb3
Natasha Ramburrun <r_natasha100@hotmail.com>
2025-06-08 13:23:37 +0100

    Added the about page and edited more of the homepage and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as ReactRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Navbar from "./components/NavBar";
 import ProductPage from "./components/ProductPage";
+import AboutPage from "./components/AboutPage";
 
 import "./styles/styles.scss";
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/Shop" element={<ProductPage />} />
+        <Route path="/About" element={<AboutPage />} />
       </Routes>
     </ReactRouter>
   );
---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,26 @@
+import image1 from "../images/AdobeStock_image.jpeg";
+
+function AboutPage() {
+  return (
+    <div className="about-page">
+      <div className="about-card">
+        <div className="about-card-image">
+          <figure className="image">
+            <img src={image1} alt="Placeholder image" />
+          </figure>
+        </div>
+        <div class="about-card">
+          <div class="about-card-content">
+            <p class="content">
+              Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at
+              eros. Donec id elit non mi porta gravida at eget metus. Cum sociis
+              natoque penatibus et magnis dis parturient montes, nascetur
+              ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AboutPage;
